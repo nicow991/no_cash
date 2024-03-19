ef70e5b0b6952b811f84e2c1bae7acb64c7b5820
guido palmili <guidopalmili@gmail.com>
2024-03-19 10:42:42 -0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, except: :index
-  resources :offers, only: [:new, :create, :edit, :update]
-  resources :deals, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+  resources :offers, only: [:new, :create, :edit, :update, :destroy]
+  resources :deals, only: [:index, :show, :create] do
     member do
       patch :cancel
     end
+    resources :reviews, only: [:create]
+
   end
   resources :chatrooms, only: [:show, :create] do
     resources :messages, only: :create
