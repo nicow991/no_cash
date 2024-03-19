64629d5ead85602d28200fc42696b244c3a8af30
guido palmili <guidopalmili@gmail.com>
2024-03-19 09:08:17 -0300

    chatrooms and messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, except: :index
-  resources :offers, only: [:new, :create]
+  resources :offers, only: [:new, :create, :edit, :update]
   resources :deals, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
     member do
       patch :cancel
     end
+  end
+  resources :chatrooms, only: [:show, :create] do
+    resources :messages, only: :create
   end
 
 end
