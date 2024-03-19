213eef2e46fda648edead203d3b9f886e2d741ad
Nicolas Waitzel <koolbuyers@gmail.com>
2024-03-19 12:05:23 -0300

    CRUD for deals_controller is done for basic usage

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,13 +1,38 @@
 class DealsController < ApplicationController
-  def create
-  end
+  before_action :set_deal, only: %i[show cancel]
 
-  def update
+  def index
+    @deals = current_user.deals
   end
 
   def show
   end
 
-  def index
+  def create
+    @deal = Deal.new(deal_params)
+    @deal.user = current_user
+    @deal.offer = Offer.find(params[:offer_id])
+    if @deal.save
+      redirect_to deals_path
+    else
+      render 'deals/index'
+    end
+  end
+
+  def cancel
+    @deal.status = "cancelled"
+    @deal.save
+    redirect_to deals_path
+  end
+
+
+  private
+
+  def deal_params
+    params.require(:deal).permit(:status)
+  end
+
+  def set_deal
+    @deal = Deal.find(params[:id])
   end
 end
