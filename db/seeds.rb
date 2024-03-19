8b64e405067e96dd0368b0e36fabd8ba3180fe6a
guido palmili <guidopalmili@gmail.com>
2024-03-19 20:25:38 -0300

    all done. Reviews could be added later

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,14 @@ CATEGORIES.each do |name|
   Category.create!(name: name)
 end
 
-22.times do |time|
+20.times do |time|
   puts "creating user"
   user = User.create!(email: "#{time}@mail.com", password: 'password', address: '123 Main St')
   5.times do
     puts "creating item"
     item = Item.create!(user: user, category: Category.all.sample, name: Faker::Book.title, description: Faker::Book.genre, condition: CONDITIONS.sample)
 
-    0.times do
+    2.times do
       puts "creating item photoo"
 
       image_url = Faker::LoremFlickr.image(size: "200x200", search_terms: ['book'])
@@ -35,7 +35,7 @@ end
 end
 
 User.first(10).each do |user|
-puts "creating user"
+puts "creating offer"
 
   1.times do |time|
     puts "creating offer for user"
@@ -45,22 +45,21 @@ puts "creating user"
   user.offers.each do |offer|
     if [1,2].sample == 1
       puts "creating deal for offer"
-
-      deal = Deal.create!(offer_id: offer.id, status: 'accepted')
-      rand = [1, 2, 3, 4].sample
-
-    #   puts "creating review deal for offer"
-
-    # #   deal.update(status: 'completed')
-    # #   Review.create!(user_reviewed_id: user.id, deal: deal, user_reviewer_id: Item.find(offer.requested_item_id).user.id, content: 'Smooth exchange')
-    # # elsif rand == 3
-    # #   deal.update(status: 'completed')
-    # #   Review.create!(user_reviewed_id: Item.find(offer.requested_item_id).user.id, deal: deal, user_reviewer_id: user.id, content: 'Great transaction')
-    # # elsif rand == 2
-    #   deal.update(status: 'completed')
-    #   Review.create!(user_reviewed_id: Item.find(offer.requested_item_id).user.id, deal: deal, user_reviewer_id: user.id, content: 'Great transaction')
-    #   Review.create!(user_reviewed_id: user.id, deal: deal, user_reviewer_id: Item.find(offer.requested_item_id).user.id, content: 'Smooth exchange')
+      Deal.create!(offer_id: offer.id, status: 'accepted')
+      # rand = [1, 2, 3, 4].sample
+      #   puts "creating review deal for offer"
+      # #   deal.update(status: 'completed')
+      # #   Review.create!(user_reviewed_id: user.id, deal: deal, user_reviewer_id: Item.find(offer.requested_item_id).user.id, content: 'Smooth exchange')
+      # # elsif rand == 3
+      # #   deal.update(status: 'completed')
+      # #   Review.create!(user_reviewed_id: Item.find(offer.requested_item_id).user.id, deal: deal, user_reviewer_id:
+      # user.id, content: 'Great transaction')
+      # # elsif rand == 2
+      #   deal.update(status: 'completed')
+      #   Review.create!(user_reviewed_id: Item.find(offer.requested_item_id).user.id, deal: deal, user_reviewer_id:
+      # user.id, content: 'Great transaction')
+      #   Review.create!(user_reviewed_id: user.id, deal: deal, user_reviewer_id:
+      # Item.find(offer.requested_item_id).user.id, content: 'Smooth exchange')
     end
-
   end
 end
