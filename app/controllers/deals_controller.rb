009452f1b34879c42291f52d5bf643aac708041c
guido palmili <guidopalmili@gmail.com>
2024-03-20 20:21:46 -0300

    logic for offers work, added to controller and to view + partial

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,7 +2,18 @@ class DealsController < ApplicationController
   before_action :set_deal, only: %i[show cancel]
 
   def index
-    @deals = Deal.where(offer_id: current_user.id)
+    # Deals the current_user started and where accepted
+    @deals = Deal.where(user_id: current_user.id)
+
+    # Find received deals where the current_user accepted the offer
+    @received_deals = Deal.joins(:offer).where(offers: { requested_item_id: current_user.items.pluck(:id) })
+
+    # Offers the current_user did
+    @my_offers = Offer.where(user_id: current_user.id)
+
+    # Find received offers by the current_user
+    @received_offers = Offer.where(requested_item_id: current_user.items.pluck(:id))
+
   end
 
   def show
