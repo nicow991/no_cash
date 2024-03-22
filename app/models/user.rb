e67c41eed409377493ff59310b635f2a6fc3576e
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-22 11:20:36 -0300

    deals offers controllers done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   has_many :chatrooms
   has_many :preferences, dependent: :destroy
   has_many :items, dependent: :destroy
-  has_many :my_offers, through: :items, dependent: :destroy, source: "offer"
-  has_many :received_offers, through: :items, dependent: :destroy, source: "offers"
+  has_many :offered_offers, through: :items, dependent: :destroy, source: :offered_offers
+  has_many :received_offers, through: :items, dependent: :destroy, source: :requested_offers
+  has_many :offered_deals, through: :offered_offers, source: :deal
+  has_many :received_deals, through: :received_offers, source: :deal
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
