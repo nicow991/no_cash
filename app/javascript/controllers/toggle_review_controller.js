a612e30053ad58d4ef80667da742346bc83e6a8e
guido palmili <guidopalmili@gmail.com>
2024-03-24 16:54:37 -0300

    added reviews and rating to the show

--- app/javascript/controllers/toggle_review_controller.js
+++ app/javascript/controllers/toggle_review_controller.js
@@ -1,15 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+  import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="toggle-review"
-export default class extends Controller {
-  static targets = [ "review" ]
+  // Connects to data-controller="toggle-review"
+  export default class extends Controller {
+    static targets = [ "review" ]
 
-  connect() {
-    console.log("Hello, Stimulus! review")
+    showreview() {
+      this.reviewTarget.classList.toggle('d-none')
+      this.reviewTarget.scrollIntoView({behavior: "smooth"});
+    }
   }
-
-  showreview() {
-    this.reviewTarget.classList.toggle('d-none')
-    this.reviewTarget.scrollIntoView({behavior: "smooth"});
-  }
-}
