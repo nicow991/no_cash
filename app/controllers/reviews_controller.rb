51d37a6c1a22e191c84e3b2b8ed9d64cc9f7277e
guido palmili <guidopalmili@gmail.com>
2024-03-23 21:52:32 -0300

    Reviews are able to create, still need to improve the view and logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,23 @@
 class ReviewsController < ApplicationController
+
   def create
     @review = Review.new(review_params)
-    @review.deal = Deal.find(params[:deal_id])
-    @review.user = current_user
-    @review.save
-    redirect_to deal_path(@review.deal)
+    @deal = Deal.find(params[:deal_id])
+    @review.deal = @deal
+    @reviewed_user = [@deal.user_offerer, @deal.user_requested].reject { |user| user == current_user }.first
+    @review.user_reviewed = @reviewed_user
+    @review.user_reviewer = current_user
+
+    if @review.save
+      redirect_to deals_path, notice: 'Reseña creada'
+    else
+      render 'deals/index', status: :unprocessable_entity
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :deal
+  belongs_to :user_reviewed, class_name: 'User', foreign_key: 'user_reviewed_id'
+  belongs_to :user_reviewer, class_name: 'User', foreign_key: 'user_reviewer_id'
   validates :rating, :content, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :deal, uniqueness: { scope: :user, message: "You have already reviewed this deal" }
+  validates :deal, uniqueness: { scope: :user_reviewer_id, message: "You have already reviewed this deal" }
+  validates :deal, uniqueness: { scope: :user_reviewer_id, message: "You have already reviewed this deal" }
   validates :content, length: { minimum: 10, maximum: 1000 }
 end
