4cf7f79ac39c4fa79fb52b8be40f1c8bba2c3ff0
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-24 09:44:22 -0300

    deleted db folder inside views folder

--- app/views/db/migrate/20240319151634_add_content_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToReviews < ActiveRecord::Migration[7.1]
-  def change
-    add_column :reviews, :content, :text
-  end
-end
--- app/views/db/migrate/20240319183911_add_nickname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNicknameToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :nickname, :string
-  end
-end
